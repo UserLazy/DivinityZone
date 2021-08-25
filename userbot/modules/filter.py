d907d8744788b5ae6857c879e5b9c9136bb81eab
DivinityZone <89197891+divinity099@users.noreply.github.com>
2021-08-25 09:18:50 +0800

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -39,7 +39,7 @@ async def filter_incoming_handler(handler):
         pass
 
 
-@register(outgoing=True, pattern=r"^ftr (.*)")
+@register(outgoing=True, pattern=r"^Ftr (.*)")
 async def add_new_filter(new_handler):
     """ For .filter command, allows adding new filters in a chat """
     try:
@@ -145,7 +145,7 @@ CMD_HELP.update({
     "filter":
     "`Fltrs`\
     \nUsage: Melihat filter RAM UBOT yang aktif di obrolan.\
-    \n\n`Fltr` <keyword> <balasan> atau balas ke pesan ketik .filter <keyword>\
+    \n\n`Ftr` <keyword> <balasan> atau balas ke pesan ketik .filter <keyword>\
     \nUsage: Membuat filter di obrolan.\
     \nBot Akan Membalas Jika Ada Yang Menyebut 'keyword' yang dibuat.\
     \nBisa dipake ke media/sticker/vn/file.\
