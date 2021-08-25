cfd4b50be4b76ff34bd88a443ebd1210352cf4d5
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-08-25 08:56:11 +0700

    core: init -> try

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -108,7 +108,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/aryazakaria01/DivinityZone")
+    "https://github.com/divinity099/DivinityZone")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "DivinityZone")
 
